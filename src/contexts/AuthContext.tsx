dcb0e98bd20d2ed32ec73a6342c50260de440071
nick-campos <nicolascampos611@gmail.com>
2026-04-30 17:01:26 -0300

    fix: retirado comentários

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,7 +2,6 @@ import { createContext, useEffect, useState } from 'react'
 import type { User } from '@supabase/supabase-js'
 import { supabase } from '../lib/supabase'
 
-// Define o "cardápio" do contexto — o que o resto do app vai encontrar quando acessar as informações de autenticação
 interface AuthContextType {
     user: User | null
     loading: boolean
@@ -11,32 +10,25 @@ interface AuthContextType {
     signOut: () => Promise<void>
 }
 
-// Aqui foi criado o canal global — os componentes vão acessar os dados de autenticação
 export const AuthContext = createContext<AuthContextType | null>(null)
 
-// É o componente que envolve o app inteiro tudo que estiver dentro dele pode acessar os dados de auth.
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-    // Duas variáveis reativas que o Provider controla quando mudam, todos os componentes que as usam re-renderizam
     const [user, setUser] = useState<User | null>(null)
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        //Verifica se já existe sessão ativa, evita o usuário ser deslogado ao atualizar a página
         supabase.auth.getSession().then(({ data: { session } }) => {
             setUser(session?.user ?? null)
             setLoading(false)
         })
 
-        //Escuta mudanças de autenticação, loga, desloga ou a sessão expira
         const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
             setUser(session?.user ?? null)
         })
 
-        // Sem isso, ele continuaria rodando em segundo plano desperdiçando memória
         return () => subscription.unsubscribe()
     }, [])
 
-    // Cada função fala com o Supabase e lança o erro pra cima caso algo dê errado — quem chamou decide o que mostrar
     const signIn = async (email: string, password: string) => {
         const {error} = await supabase.auth.signInWithPassword({email, password})
         if (error) throw error
@@ -52,7 +44,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         if(error) throw error
     }
 
-    // Disponibiliza todos os dados e funções para os filhos.
     return (
         <AuthContext.Provider value={{user, loading, signIn, signUp, signOut}}>
             {children}
